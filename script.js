4b71ea76678958d22452437a936179b95e5d4616
Kavya <kavya.aswadhati@gmail.com>
2024-10-14 18:26:23 -0700

    gallery handling

---
+++ script.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.querySelector('.card-inner').classList.toggle('flipped');
+  });
+});
